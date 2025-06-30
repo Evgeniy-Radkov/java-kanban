4de85d8c31d26dd8e8ae10d4f08f0ed9a4f146e0
Evgeniy Radkov <boss.radkov2000@yandex.ru>
2025-06-30 16:36:56 +0300

    Fix: checkStyle3

--- src/server/handler/BaseHttpHandler.java
+++ src/server/handler/BaseHttpHandler.java
@@ -21,6 +21,7 @@ public abstract class BaseHttpHandler implements HttpHandler {
                 .registerTypeAdapter(LocalDateTime.class, new LocalDateTimeAdapter())
                 .create();
     }
+
     
     protected final TaskManager manager;
     protected final Gson gson;
